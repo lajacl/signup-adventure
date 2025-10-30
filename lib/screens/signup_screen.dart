0bf06358c4bc6c22cf40579fa71d1a330cf2c225
lajacl <lavette.clifton@gmail.com>
2025-10-29 23:32:32 -0400

    Fix render error with animated container

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -295,7 +295,7 @@ class _SignupScreenState extends State<SignupScreen> {
                 // Submit Button w/ Loading Animation
                 AnimatedContainer(
                   duration: const Duration(milliseconds: 300),
-                  width: _isLoading ? 60 : double.infinity,
+                  width: double.infinity,
                   height: 60,
                   child: _isLoading
                       ? const Center(
